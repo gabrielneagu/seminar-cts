a7f2f2a206deec7d05a7b89235f3ade36e969bfb
gabin <gabin@DESKTOP-QT8IGUV>
2021-03-04 17:15:32 +0200

    AplicantReader

---
+++ seminar2/src/ro/ase/cts/clase/reader/AplicantReader.java
@@ -0,0 +1,18 @@
+package ro.ase.cts.clase.reader;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import ro.ase.cts.clase.Aplicant;
+
+public abstract class AplicantReader {
+
+	protected String fileName;
+	
+	public AplicantReader(String fileName) {
+		super();
+		this.fileName = fileName;
+	}
+
+	public abstract List<Aplicant> readAplicanti() throws FileNotFoundException;
+}
